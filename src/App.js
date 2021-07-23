7fccf1834b79d2bb8cc393352e5189c38491c523
Jamilly Freitas <jamirodrifrei@gmail,com>
2021-07-22 22:19:17 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Teste from './Componentes/teste';
+
+function App() {
+  return (
+    <Teste />
+  );
+}
+
+export default App;
---
+++ src/Componentes/teste.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Teste extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Livros </h1>
+            </div>
+        );
+    }
+}
+
+export default Teste;
